8bd2137d5258bcd1d7e4c28992aeeb039366b699
Rushil Lakhani Lakhani <alu4093@cc190.(none)>
2013-11-07 15:29:56 +0000

    Añadiendo modificacion

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -6,6 +6,12 @@ describe Matriz do
   before :each do
     @mat1 = Matriz.new(2,2,[[4,1],[5,7]])
     @mat2 = Matriz.new(2,2,[[2,1],[3,5]])
+    frac1 = Fraccion.new(1,3)
+    frac2 = Fraccion.new(1,4)
+    frac3 = Fraccion.new(1,5)
+    frac4 = Fraccion.new(1,6)  
+    @mat1_frac = Matriz.new(2,2,[[frac1,frac2],[frac3,frac4]])
+    @mat2_frac = Matriz.new(2,2,[[frac1,frac2],[frac3,frac4]])
   end
 
 # Expectativa que comprueba el numero de filas de una matriz  
@@ -61,7 +67,35 @@ describe Matriz do
     mul.to_s.should eq("  [ 11 9 ]\n  [ 31 40 ]\n ")
     
   end
+  
+# Expectativa que comprueba que la matriz de racional se muestra correctamente
+  
+     it "Comprobando que la matriz de racional se muestra correctamente 2x2 " do
+    @mat1_frac.to_s.should eq ("  [ 1/3 1/4 ]\n  [ 1/5 1/6 ]\n ")
+   end
+   
+# Expectativa que comprueba que la matrices de racionales se suman correctamente
+  
+   it "Comprobando que las matrices de racionales se suman correctamente " do
+      sum = @mat1_frac+@mat2_frac
+      sum.to_s.should eq ("  [ 2/3 1/2 ]\n  [ 2/5 1/3 ]\n ")
+    end
 
+# Expectativa que comprueba que la matrices de racionales se restan correctamente
+  
+    it "Comprobando que las matrices de racionales se restan correctamente " do
+      res = @mat1_frac-@mat2_frac
+      res.to_s.should eq ("  [ 0/1 0/1 ]\n  [ 0/1 0/1 ]\n ")
+    end
+  
+=begin   
+# Expectativa que comprueba que la matrices de racionales se multiplican correctamente
+  
+    it "Comprobando que las matrices de racionales se multiplican correctamente " do
+      sum = @mat1_frac*@mat2_frac
+      sum.to_s.should eq ("  [ 0/1 0/1 ]\n  [ 0/1 0/1 ]\n ")
+    end    
+=end
 
   
 end
